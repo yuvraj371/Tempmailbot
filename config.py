dd7378d3189a0195958b7ac40f20df336e2099c0
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-07-23 18:28:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 
-API_ID =   # Replace with your API ID
-API_HASH = ""  # Replace with your API hash
-BOT_TOKEN = ""  # Replace with your bot token
+API_ID = 21938068 # Replace with your API ID
+API_HASH = "c18fd98f3e58484df0aecd95a3d5a6a9"  # Replace with your API hash
+BOT_TOKEN = "6230727877:AAHeaWc601jvL-lKf5VLI04Efet10SFx_GE"  # Replace with your bot token
